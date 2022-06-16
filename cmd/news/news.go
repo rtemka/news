b26cf61e376b83be2ab55d422ea8fa2628b0ce79
Artem Rybakov <gizmo3000@yandex.ru>
2022-06-16 13:39:33 +0300

    add config reading

--- cmd/news/news.go
+++ cmd/news/news.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// config - структура для хранения конфигурации
+// передаваемой в качестве аргумента коммандной строки
+type config struct {
+	Links        []string `json:"links"`         // массив ссылок для опроса
+	SurveyPeriod int      `json:"survey_period"` // период опроса ссылок
+}
+
+// readConfig функция для чтения файла конфигурации
+func readConfig(path string) (*config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var c config
+
+	return &c, json.NewDecoder(f).Decode(&c)
+}
+
 func main() {
+	if len(os.Args) == 1 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <path-to-config-file>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	_, err := readConfig(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 
 }
