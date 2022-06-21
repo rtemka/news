9834dcde463f4d1ada8d370f84b8c45b8bffaa28
Artem Rybakov <gizmo3000@yandex.ru>
2022-06-21 15:08:57 +0300

    add db in memory

--- pkg/storage/memdb/memdb.go
+++ pkg/storage/memdb/memdb.go
@@ -1,1 +1,41 @@
 package memdb
+
+import "news/pkg/storage"
+
+type MemDB struct{}
+
+func New() *MemDB {
+	return &MemDB{}
+}
+
+var sampleItem = storage.Item{
+	Id:          1,
+	PubDate:     5555555,
+	Title:       "sample item",
+	Description: "sample discription",
+	Link:        "https://test.com",
+}
+
+func (db *MemDB) Item(id int) (storage.Item, error) {
+	return sampleItem, nil
+}
+
+func (db *MemDB) Items(id int) ([]storage.Item, error) {
+	return []storage.Item{sampleItem, sampleItem}, nil
+}
+
+func (db *MemDB) AddItem(_ storage.Item) error {
+	return nil
+}
+
+func (db *MemDB) DeleteItem(_ storage.Item) error {
+	return nil
+}
+
+func (db *MemDB) UpdateItem(_ storage.Item) error {
+	return nil
+}
+
+func (db *MemDB) Close() error {
+	return nil
+}
