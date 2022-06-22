63e38a61c59e5d7fc24ff9b950363bf760133e3d
Artem Rybakov <gizmo3000@yandex.ru>
2022-06-22 17:25:08 +0300

    change memdb to conform Storage interface

--- pkg/storage/memdb/memdb.go
+++ pkg/storage/memdb/memdb.go
@@ -1,6 +1,9 @@
 package memdb
 
-import "news/pkg/storage"
+import (
+	"context"
+	"news/pkg/storage"
+)
 
 type MemDB struct{}
 
@@ -16,23 +19,27 @@ var sampleItem = storage.Item{
 	Link:        "https://test.com",
 }
 
-func (db *MemDB) Item(id int) (storage.Item, error) {
+func (db *MemDB) Item(_ context.Context, _ string) (storage.Item, error) {
 	return sampleItem, nil
 }
 
-func (db *MemDB) Items(id int) ([]storage.Item, error) {
+func (db *MemDB) Items(_ context.Context, _ int) ([]storage.Item, error) {
 	return []storage.Item{sampleItem, sampleItem}, nil
 }
 
-func (db *MemDB) AddItem(_ storage.Item) error {
+func (db *MemDB) AddItem(_ context.Context, _ storage.Item) error {
 	return nil
 }
 
-func (db *MemDB) DeleteItem(_ storage.Item) error {
+func (db *MemDB) AddItems(_ context.Context, _ []storage.Item) error {
 	return nil
 }
 
-func (db *MemDB) UpdateItem(_ storage.Item) error {
+func (db *MemDB) DeleteItem(_ context.Context, _ storage.Item) error {
+	return nil
+}
+
+func (db *MemDB) UpdateItem(_ context.Context, _ storage.Item) error {
 	return nil
 }
 
