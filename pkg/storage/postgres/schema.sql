f20f3ede08d74e84341d117ece51b7cd4126cfff
Artem Rybakov <gizmo3000@yandex.ru>
2022-06-28 14:49:52 +0300

    add NOT NULL to description and link

--- pkg/storage/postgres/schema.sql
+++ pkg/storage/postgres/schema.sql
@@ -4,13 +4,13 @@ DROP TABLE IF EXISTS news;
 CREATE TABLE IF NOT EXISTS news (
     id BIGSERIAL PRIMARY KEY,
     title TEXT NOT NULL,
-	description TEXT,
+	description TEXT NOT NULL,
     pub_date BIGINT CHECK(pub_date > 0) DEFAULT extract(epoch from now()),
 
     -- Согласно RSS 2.0 у новости(item) есть три обязательных атрибута 
     -- (title, description и link).
     -- link - хороший кандидат в качестве ключа поиска новости.
-    link TEXT UNIQUE
+    link TEXT NOT NULL UNIQUE
 );
 
 -- индекс для атрибута pub_date.
